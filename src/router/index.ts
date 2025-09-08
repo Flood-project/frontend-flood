9e636051ef87e9663bf3a0fc7e3770204148831d
arthur-psp <apspolti@gmail.com>
2025-09-08 19:16:09 -0300

    :sparkles: feat: adiciona middleware

--- src/router/index.ts
+++ src/router/index.ts
@@ -9,19 +9,41 @@ const router = createRouter({
         {
             path: '/login',
             name: 'Login',
-            component: LoginPage
+            component: LoginPage,
+            meta: {
+                requiresAuth: false
+            }
         },
         {
             path: '/users',
             name: 'User',
-            component: UserPage
+            component: UserPage,
+            meta: {
+                requiresAuth: true
+            }
         },
         {
             path: '/logado',
             name: 'Logado',
-            component: Usuariologado
+            component: Usuariologado,
+            meta: {
+                requiresAuth: true
+            }
         },
     ]
+})
+
+router.beforeEach((to, from, next) => {
+    if (to.meta.requiresAuth) {
+        const token = localStorage.getItem('token');
+        if (token) {
+            next()
+        } else {
+            next('/login')
+        }
+    } else {
+        next();
+    }
 })
 
 export { router }
